99e566926209832bb33d75082684ec65942c4d84
Jasper Van Zeir <jaspervanzeir1@gmail.com>
2025-05-18 12:47:54 +0200

    Java Project made with Spring Boot

---
+++ ITConference/src/test/java/com/hogent/ewdj/itconference/ItConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogent.ewdj.itconference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItConferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
