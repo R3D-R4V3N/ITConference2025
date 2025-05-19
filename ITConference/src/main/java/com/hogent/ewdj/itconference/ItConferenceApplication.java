036450ae85af7c3113d7b0a1ea04df68d48e7c5f
Jasper Van Zeir <jaspervanzeir1@gmail.com>
2025-05-19 20:23:07 +0200

    REST working

--- ITConference/src/main/java/com/hogent/ewdj/itconference/ItConferenceApplication.java
+++ ITConference/src/main/java/com/hogent/ewdj/itconference/ItConferenceApplication.java
@@ -11,29 +11,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import service.FavoriteService;
-import service.FavoriteServiceImpl;
-// Verwijder: import perform.PerformRestITConference;
+import service.FavoriteServiceImpl; // Zorg dat dit 'service.impl.FavoriteServiceImpl' is, niet 'service.FavoriteServiceImpl'
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.hogent.ewdj.itconference", "domain", "exceptions", "repository", "service", "service", "util", "validator", "com.hogent.ewdj.itconference.advice", "com.hogent.ewdj.itconference.controller"}) // Verwijder "perform"
+// Pas deze regel aan: voeg "perform" toe aan de basePackages
+@ComponentScan(basePackages = {"com.hogent.ewdj.itconference", "domain", "exceptions", "repository", "service", "service", "util", "validator", "com.hogent.ewdj.itconference.advice", "com.hogent.ewdj.itconference.controller", "perform"})
 @EnableJpaRepositories({"repository"})
 @EntityScan("domain")
 public class ItConferenceApplication /* implements WebMvcConfigurer */ {
 
     public static void main(String[] args) {
         SpringApplication.run(ItConferenceApplication.class, args);
-
-        // Verwijder dit hele blok:
-        /*
-        try {
-            // Geef de server even de tijd om te starten (dit is een simpele benadering, geen garantie)
-            Thread.sleep(5000); // Wacht 5 seconden
-            new PerformRestITConference();
-        } catch (Exception e) {
-            System.err.println("Fout bij het uitvoeren van de REST client: " + e.getMessage());
-            e.printStackTrace();
-        }
-        */
     }
 
     @Bean
